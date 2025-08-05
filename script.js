2e818e851e7569e424072478117cb4574d9cae36
Mark0Velov <kontarevtimofej@gmail.com>
2025-08-05 16:38:11 +0300

    rrrrr

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+// Кастомный курсор
+
+const cursor = document.querySelector('.custom-cursor');
+
+document.addEventListener('mousemove', (e) => {
+  cursor.style.left = e.clientX + 'px';
+  cursor.style.top = e.clientY + 'px';
+});
+
+document.querySelectorAll('a, button').forEach(el => {
+  el.addEventListener('mouseenter', () => {
+    cursor.classList.add('cursor-hover');
+  });
+  el.addEventListener('mouseleave', () => {
+    cursor.classList.remove('cursor-hover');
+  });
+});
+
+
+
