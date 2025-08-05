32081c5c7b18b4701fe19b98e567e6d84ac72baa
Mark0Velov <kontarevtimofej@gmail.com>
2025-08-05 16:42:34 +0300

    ккк

--- script.js
+++ script.js
@@ -8,14 +8,3 @@ document.addEventListener('mousemove', (e) => {
   cursor.style.top = e.clientY + 'px';
 });
 
-document.querySelectorAll('a, button').forEach(el => {
-  el.addEventListener('mouseenter', () => {
-    cursor.classList.add('cursor-hover');
-  });
-  el.addEventListener('mouseleave', () => {
-    cursor.classList.remove('cursor-hover');
-  });
-});
-
-
-
