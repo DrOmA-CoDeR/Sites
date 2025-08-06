2142f8097e2ac51de162bc7eb12f2f9a7f3de93d
DrOmA-CoDeR <vasakaogon@gmail.com>
2025-08-06 14:44:49 +0300

    ув

--- script.js
+++ script.js
@@ -3,31 +3,7 @@ const ADMIN_CREDENTIALS = {
     login: "admin",
     password: "rzd2023"
 };
-const CORRECT_HASH = "9c53b67b20f097a6c7f0375e6629ea573505f60e92605f64c6945ed9d7a14697"; // Хеш от пароля "123"
-async function checkPassword() {
-  const input = document.getElementById("passwordInput").value;
-  
-  // Хешируем введённый пароль
-  const inputHash = await hashPassword(input);
-  
-  // Сравниваем с правильным хешем
-  if (inputHash === CORRECT_HASH) {
-    alert("Доступ разрешён!");
-    // Здесь можно перенаправить на защищённую страницу
-  } else {
-    alert("Неверный пароль!");
-  }
-}
 
-// Функция для хеширования (SHA-256)
-async function hashPassword(password) {
-  const encoder = new TextEncoder();
-  const data = encoder.encode(password);
-  const hashBuffer = await crypto.subtle.digest("SHA-256", data);
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
-  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-  return hashHex;
-}
 
 // Элементы
 const adminOverlay = document.getElementById('adminOverlay');
